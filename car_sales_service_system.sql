291ea7b361c953e3e486442d463d08bac106ec7e
RAMANATHAN-19 <148554695+RAMANATHAN-19@users.noreply.github.com>
2023-10-21 09:05:21 +0530

    Add files via upload

--- car_sales_service_system.sql
+++ car_sales_service_system.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.3
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 29, 2019 at 03:19 PM
--- Server version: 5.7.20
--- PHP Version: 7.3.9
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2023 at 12:02 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `car_sales_service_system`
@@ -26,7 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `django_migrations`
 --
 
-CREATE TABLE IF NOT EXISTS `django_migrations` (
+CREATE TABLE `django_migrations` (
   `id` int(11) NOT NULL,
   `app` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -39,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `django_migrations` (
 -- Table structure for table `django_session`
 --
 
-CREATE TABLE IF NOT EXISTS `django_session` (
+CREATE TABLE `django_session` (
   `session_key` varchar(40) NOT NULL,
   `session_data` longtext NOT NULL,
   `expire_date` datetime NOT NULL
@@ -55,6 +57,7 @@ INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALU
 ('3fhnq49ugv22tasfdaa893ab9qcrpuqe', 'NzU0MmFkYzI2MWY3N2Y0ODQyMDZlZTgyOWIyZWFlMDU3NTZiZjE4ODp7Im9yZGVyX2lkIjowLCJhdXRoZW50aWNhdGVkIjpmYWxzZSwidXNlcl9pZCI6ZmFsc2UsInVzZXJfbmFtZSI6ZmFsc2V9', '2019-12-09 11:40:34'),
 ('46wha6ymlpepexdfgahlgaco3u3fw3w8', 'MWIzNDJiMDQ3YjZiZDYzOTJiZjgxNDMxNDNjZWMxMWM4NWU1YjdhMDp7ImF1dGhlbnRpY2F0ZWQiOmZhbHNlLCJ1c2VyX2lkIjpmYWxzZSwidXNlcl9uYW1lIjpmYWxzZX0=', '2017-10-25 17:04:56'),
 ('66f0rt5d0m8993d3k9vf35r9qao0bn1b', 'MWIzNDJiMDQ3YjZiZDYzOTJiZjgxNDMxNDNjZWMxMWM4NWU1YjdhMDp7ImF1dGhlbnRpY2F0ZWQiOmZhbHNlLCJ1c2VyX2lkIjpmYWxzZSwidXNlcl9uYW1lIjpmYWxzZX0=', '2019-11-20 11:04:37'),
+('ae1tdmhbtctn4os5mpxsikack80nngpu', 'NzU0MmFkYzI2MWY3N2Y0ODQyMDZlZTgyOWIyZWFlMDU3NTZiZjE4ODp7Im9yZGVyX2lkIjowLCJhdXRoZW50aWNhdGVkIjpmYWxzZSwidXNlcl9pZCI6ZmFsc2UsInVzZXJfbmFtZSI6ZmFsc2V9', '2023-11-03 14:19:59'),
 ('c6n3y5oqkw8p8uvdyqzhz8vs7h1thfkz', 'NzZjODgzZDQyNmI1NDc3YTlkMjMyZWM2NjRlYTMxMzViNTE3OTU5NDp7Im9yZGVyX2lkIjowLCJ1c2VyX2lkIjoxLCJhdXRoZW50aWNhdGVkIjp0cnVlLCJ1c2VyX25hbWUiOiJBbWl0IEt1bWFyIn0=', '2019-11-27 09:37:00'),
 ('djlk6utlynky27cg727uz9kmslrsdhcb', 'OWJkNTkwNWI0ZWM3MTExYThmYzYyYTU4YTE0MTI0NDkzOWZjMDQzNzp7InVzZXJfaWQiOjE2LCJ1c2VyX2xldmVsX2lkIjo0LCJhdXRoZW50aWNhdGVkIjp0cnVlLCJ1c2VyX25hbWUiOiJBbG9rIEt1bWFyIn0=', '2017-09-10 12:08:07'),
 ('i5hzw3khnumvd6i2dbd7tu4u8txrump5', 'ZTlhMDk4NTZmMjdmOWJhMTJjMTQwODdjMDhiOWQwOWIwMzI3OGYxZjp7InVzZXJfaWQiOjEsImF1dGhlbnRpY2F0ZWQiOnRydWUsInVzZXJfbmFtZSI6IkFtaXQgS3VtYXIifQ==', '2019-11-06 08:40:52'),
@@ -69,30 +72,32 @@ INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALU
 -- Table structure for table `order`
 --
 
-CREATE TABLE IF NOT EXISTS `order` (
+CREATE TABLE `order` (
   `order_id` int(11) NOT NULL,
   `order_customer_name` varchar(255) NOT NULL,
   `order_customer_mobile` varchar(255) NOT NULL,
   `order_date` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `order`
 --
 
 INSERT INTO `order` (`order_id`, `order_customer_name`, `order_customer_mobile`, `order_date`) VALUES
-(1, 'Kaushal Kishore', '8376986802', 'ddddd'),
-(2, 'Atul Kumar', '8978675645', 'ddddd'),
-(3, 'Sarvesh', '1234543223432', 'ddddd'),
-(4, 'Radhika', '`12345678', 'ddddd'),
-(5, 'Ram Kapoor', '8470010001', 'ddddd'),
-(6, 'Ram Kapoor', '8470010001', 'ddddd'),
-(7, 'Ram Kapoor', '8470010001', 'ddddd'),
-(8, 'fghjkl', '8470010001', 'ddddd'),
-(9, 'Ram Kapoor', '8470010001', 'ddddd'),
-(10, 'Ram Kapoor', '8470010001', 'ddddd'),
-(11, 'Ram Kapoor', '8470010001', 'ddddd'),
-(12, 'Jay', '94385743895', 'ddddd');
+(1, 'Rajesh', '8376986802', 'ddddd'),
+(2, 'Ramanathan', '8978675645', 'ddddd'),
+(3, 'Ram', '8056805680', 'ddddd'),
+(4, 'Rajalakshmi', '9080908090', 'ddddd'),
+(5, 'Durairaj', '8470010001', 'ddddd'),
+(6, 'Rahul dravid', '8470010001', 'ddddd'),
+(7, 'Virat kohli', '3677967736', 'ddddd'),
+(8, 'Alex pandian', '8470010001', 'ddddd'),
+(9, 'Rajee', '8470010001', 'ddddd'),
+(10, 'Ms Dhoni', '8470010001', 'ddddd'),
+(11, 'Vijay', '8470010001', 'ddddd'),
+(12, 'Selvin', '94385743895', 'ddddd'),
+(13, 'CM', '108', 'ddddd'),
+(14, 'Virat kohli', '9519519511', 'ddddd');
 
 -- --------------------------------------------------------
 
@@ -100,12 +105,12 @@ INSERT INTO `order` (`order_id`, `order_customer_name`, `order_customer_mobile`,
 -- Table structure for table `order_item`
 --
 
-CREATE TABLE IF NOT EXISTS `order_item` (
+CREATE TABLE `order_item` (
   `oi_id` int(11) NOT NULL,
   `oi_order_id` int(11) NOT NULL,
   `oi_product_id` int(11) NOT NULL,
   `oi_cost` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `order_item`
@@ -137,28 +142,30 @@ INSERT INTO `order_item` (`oi_id`, `oi_order_id`, `oi_product_id`, `oi_cost`) VA
 -- Table structure for table `product_product`
 --
 
-CREATE TABLE IF NOT EXISTS `product_product` (
+CREATE TABLE `product_product` (
   `product_id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_cost` varchar(255) NOT NULL,
   `product_type` varchar(100) NOT NULL,
   `product_company` varchar(100) NOT NULL,
   `product_stock` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product_product`
 --
 
 INSERT INTO `product_product` (`product_id`, `product_name`, `product_cost`, `product_type`, `product_company`, `product_stock`) VALUES
-(1, 'Tyres', '400', 'Tyres', 'MRF', '100'),
-(2, 'Sockets', '500', 'Electric Part', 'TVS', '100'),
-(3, 'Bonets', '80', 'Car Parts', 'TVS', '13'),
-(4, 'Oil Filter', '100', 'Oiling', 'TVS', '2'),
-(5, 'Brush', '200', 'Cleaning', 'TVS', '2'),
-(6, 'Wipers', '300', 'Car Parts', 'TVS', '12'),
-(7, 'Side Mirror', '200', 'Accessories', 'TVS', '12'),
-(8, 'Seat Cover', '250', 'Accessories', 'TVS', '11');
+(1, 'Thar', '4,00,000', 'Petrol', 'Mahindra', '2'),
+(2, 'MG Hector', '11,00,000', 'Petrol', 'Morris Garages', '1'),
+(3, 'Swift', '1,50,000', 'Diesel', 'Maruti Suzuki', '5'),
+(4, 'Scorpio', '5,50,000', 'Diesel', 'Mahindra', '2'),
+(5, 'Fortuner', '13,00,000', 'Petrol', 'Toyato', '2'),
+(6, 'Verna', '7,00,000', 'Diesel', 'Hyundai', '4'),
+(7, 'City', '8,00,000', 'Petrol', 'Honda', '2'),
+(8, 'XUV 700', '7,00,000', 'Diesel', 'Mahindra', '7'),
+(9, 'Innova', '15,00,000', '2.7G', 'Toyato', '2'),
+(10, 'Rapid', '5,00,000', 'Petrol', 'skoda', '1');
 
 -- --------------------------------------------------------
 
@@ -166,21 +173,21 @@ INSERT INTO `product_product` (`product_id`, `product_name`, `product_cost`, `pr
 -- Table structure for table `users_user`
 --
 
-CREATE TABLE IF NOT EXISTS `users_user` (
+CREATE TABLE `users_user` (
   `user_id` int(11) NOT NULL,
   `user_username` varchar(255) NOT NULL,
   `user_password` varchar(255) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_mobile` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `users_user`
 --
 
 INSERT INTO `users_user` (`user_id`, `user_username`, `user_password`, `user_name`, `user_email`, `user_mobile`) VALUES
-(1, 'admin', 'test', 'Amit Kumar', 'amit@gmail.com', '9878987678');
+(1, 'admin', 'admin', 'Amit Kumar', 'amit@gmail.com', '9878987678');
 
 --
 -- Indexes for dumped tables
@@ -232,26 +239,32 @@ ALTER TABLE `users_user`
 --
 ALTER TABLE `django_migrations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
 --
 -- AUTO_INCREMENT for table `order_item`
 --
 ALTER TABLE `order_item`
-  MODIFY `oi_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=49;
+  MODIFY `oi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
 --
 -- AUTO_INCREMENT for table `product_product`
 --
 ALTER TABLE `product_product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `users_user`
 --
 ALTER TABLE `users_user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
